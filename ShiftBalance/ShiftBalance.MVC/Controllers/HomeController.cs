70470332850596810f5c46e0b323b3fc161b9738
Sebastiano Pighi <sebastiano.pighi@gmail.com>
2024-06-24 21:16:36 +0200

    Creazione del progetto MVC

---
+++ ShiftBalance/ShiftBalance.MVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using ShiftBalance.MVC.Models;
+using System.Diagnostics;
+
+namespace ShiftBalance.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
